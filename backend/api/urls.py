b395ea04995866761a38b5ef00d463e1c045b0fd
donj808 <donj.barit@gmail.com>
2025-09-28 15:30:56 -0700

    remove unused endpoints

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,11 +8,11 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('register/', users.register_user, name='register_user'),
     path('profile/', users.get_user_profile, name='get_user_profile'),
+    
     # JWT authentication endpoints
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('plans/', plans.get_plans, name='get-plans'),
-    path('plans/add', plans.create_plan, name='create-plan'),
-    path('plans/<str:plan_id>', plans.get_plans_by_id, name='get-plans-by-id'),
-    path('plans/<str:plan_id>/edit', plans.update_plan, name='update-plan'),
+    
+    # Plans endpoints
+    path('plans/add', plans.create_plan, name='create-plan')
 ]
