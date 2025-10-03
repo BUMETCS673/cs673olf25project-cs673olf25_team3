18caeb56ef85b99abd3b6ae5425eda5ffb078321
Haolin Yang <haoliny@bu.edu>
2025-10-02 21:06:41 -0400

    Added create, delete and get for RSVP

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import plans, users
+from .views import plans, users, rsvp
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 # Framework-generated: 5%
@@ -19,4 +19,11 @@ urlpatterns = [
     path('plans/<str:plan_id>', plans.get_plans_by_id, name='get-plans-by-id'),
     path('plans/<str:plan_id>/edit', plans.update_plan, name='update-plan'),
     path('plans/<str:plan_id>/delete', plans.delete_plan, name='delete-plan'),
+
+    # RSVP endpoints
+    path('rsvp/add', rsvp.create_rsvp, name='create-rsvp'),
+    path('rsvp/<str:plan_id>', rsvp.get_rsvp_by_plan_id, name='get-rsvp-by-plan-id'),
+    path('rsvp/<str:user_id>', rsvp.get_rsvp_by_user_id, name='get-rsvp-by-user-id'),
+    path('rsvp/<str:rsvp_id>/delete', rsvp.delete_rsvp_by_id, name='delete-rsvp-by-id'),
+    path('rsvp/<str:plan_id>/delete/plan', rsvp.delete_rsvp_by_plan_id, name='delete-rsvp-by-plan-id'),
 ]
