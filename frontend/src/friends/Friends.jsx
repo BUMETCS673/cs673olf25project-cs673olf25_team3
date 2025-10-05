84eba297c4bc297629fb2244308e1a70d0bf4729
David <dmetraux@isti.com>
2025-10-05 19:03:21 -0400

    added a jank way to make it update

--- frontend/src/friends/Friends.jsx
+++ frontend/src/friends/Friends.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import FriendsList from "./FriendsList.jsx";
+// import FriendsList from "./FriendsList.jsx";
 import User from '../util.js';
 import { useEffect, useState } from "react";
 import { useAuth } from "../auth/AuthContext";
 import { getUsers } from "./endpoints/getUsers.js";
 import { getFriends } from "./endpoints/getFriends.js";
 import { GetUserByID } from "./endpoints/getUserByID.js";
+import { SendFriendRequest } from "./endpoints/sendFriendRequest.js";
+import { DeleteFriend } from "./endpoints/deleteFriend.js";
+import { RespondToFriendRequest } from "./endpoints/respondToFriendRequest.js";
+import { Button } from "@mui/material";
 
 
 export default function Friends() {
@@ -21,6 +25,7 @@ export default function Friends() {
   const { auth } = useAuth();
   const [allUsers, setAllUsers] = useState([]);
   const [unconnectedUsers, setUnconnectedUsers] = useState([])
+  const [flip, setFlip]= useState(false)
   const [friends, setFriends] = useState([]);
 
 
@@ -49,27 +54,61 @@ export default function Friends() {
   useEffect(() => {
     loadAllUsers();
     loadFriends();
-  }, [auth.accessToken]);
+    setFlip(false)
+  }, [auth.accessToken, flip]);
 
   useEffect(() => {
-    let filteredUsers = allUsers.slice();
+    if (allUsers.length > 0){
+      var filteredUsers = allUsers.slice();
     
-    let excludedIds = [friends.current_user_id]
-    if (friends.friends){
-      let currentFriendIds = friends.friends.map(friend => friend.id);
-      excludedIds= excludedIds.concat(currentFriendIds)
+      var excludedIds = [friends.current_user_id]
+      if (friends.friends){
+        var currentFriendIds = friends.friends.map(friend => friend.id);
+        excludedIds= excludedIds.concat(currentFriendIds)
+      }
+      if (friends.incoming_requests) {
+        var receivedRequestIds = friends.incoming_requests.map(user => user.id);
+        excludedIds = excludedIds.concat(receivedRequestIds)
+      }
+      if (friends.outgoing_requests){
+        var sentRequestIds = friends.outgoing_requests.map(user => user.id);
+        excludedIds =excludedIds.concat(sentRequestIds)
+      }
+      filteredUsers  = filteredUsers.filter((user) => !(excludedIds.includes(user.id)))
+      setUnconnectedUsers(filteredUsers)
     }
-    if (friends.incoming_requests) {
-      let receivedRequestIds = friends.incoming_requests.map(user => user.id);
-      excludedIds = excludedIds.concat(receivedRequestIds)
-    }
-    if (friends.outgoing_requests){
-      let sentRequestIds = friends.outgoing_requests.map(user => user.id);
-      excludedIds =excludedIds.concat(sentRequestIds)
-    }
-    filteredUsers  = filteredUsers.filter((user) => {!(user.id in excludedIds)})
-    setUnconnectedUsers(filteredUsers)
+
   }, [allUsers, friends])
+
+    function VariantButtons({variant, userID}){
+
+      switch (variant) {
+      case "current":
+        return <Button onClick={()=>{DeleteFriend(userID, auth.accessToken); setFlip(true)}}>Remove</Button>
+      case "receive":
+        return  <><Button onClick={()=>{RespondToFriendRequest(userID, 'accept', auth.accessToken); setFlip(true)}}>Accept</Button><Button onClick={()=>{RespondToFriendRequest(userID, 'reject', auth.accessToken); loadFriends()}}>Ignore</Button></>
+      case "send":
+        return  <Button onClick={()=>{SendFriendRequest(userID, auth.accessToken); setFlip(true)}}>Send</Button>
+      default:
+        return 
+      }
+    }
+
+    function FriendsList({friends, variant}) {
+
+      var listItems;
+      if (friends){
+          listItems = friends.map(friend =>
+          <li key={friend.id}>
+            {friend.username}
+            <VariantButtons variant={variant} userID={friend.id}/>
+          </li>
+          )
+      }
+      return (
+            <ul>{listItems}</ul>
+        )
+    }
 
   
 
@@ -79,22 +118,22 @@ export default function Friends() {
 
       <div>
         <h2>Current Friends:</h2>
-        <FriendsList friends={friends.friends} variant={"current"} loadFriends={loadFriends}></FriendsList>
+        <FriendsList friends={friends.friends} variant={"current"}></FriendsList>
       </div>
 
       <div>
         <h2>Add Friends:</h2>
-        <FriendsList friends={unconnectedUsers}  variant={"send"} loadFriends={loadFriends}></FriendsList>
+        <FriendsList friends={allUsers}  variant={"send"}></FriendsList>
       </div>
 
       <div>
         <h2>Received Friend Requests:</h2>
-        <FriendsList friends={friends.incoming_requests} variant={"receive"} loadFriends={loadFriends}></FriendsList>
+        <FriendsList friends={friends.incoming_requests} variant={"receive"}></FriendsList>
       </div>
 
       <div>
         <h2>Sent Friend Requests:</h2>
-        <FriendsList friends={friends.outgoing_requests} variant={null} loadFriends={loadFriends}></FriendsList>
+        <FriendsList friends={friends.outgoing_requests} variant={null}></FriendsList>
       </div>
     </div>
   );
