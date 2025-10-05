5b9daae34ff25171d99d000cfc4fc3c83d98b08e
David <dmetraux@isti.com>
2025-10-05 14:20:23 -0400

    adding api calls

--- frontend/src/friends/Friends.jsx
+++ frontend/src/friends/Friends.jsx
@@ -4,21 +4,23 @@ import User from '../util.js';
 import { useEffect, useState } from "react";
 import { useAuth } from "../auth/AuthContext";
 import { getUsers } from "./endpoints/getUsers.js";
+import { getFriends } from "./endpoints/getFriends.js";
 
 
 export default function Friends() {
-    var myself =  new User("David");
+  //   var myself =  new User("David");
 
-    const friends = [].filter(user =>
-    user.isFriends(myself)
-  );
+  //   const friends = [].filter(user =>
+  //   user.isFriends(myself)
+  // );
 
-  const notFriends = [].filter(user =>
-    !user.isFriends(myself)
-  );
+  // const notFriends = [].filter(user =>
+  //   !user.isFriends(myself)
+  // );
 
   const { auth } = useAuth();
   const [users, setUsers] = useState([]);
+  const [friends, setFriends] = useState([]);
   const [loading, setLoading] = useState(true);
 
 
@@ -36,8 +38,21 @@ export default function Friends() {
     setLoading(false);
   };
 
+  const loadFriends = async () => {
+    if (!auth.accessToken) return;
+    setLoading(true);
+    const result = await getFriends(auth.accessToken);
+    if (!result.errorMessage) {
+      setFriends(result);
+    } else {
+      console.error(result.errorMessage);
+    }
+    setLoading(false);
+  };
+
   useEffect(() => {
     loadUsers();
+    loadFriends();
   }, [auth.accessToken]);
 
   
@@ -48,17 +63,19 @@ export default function Friends() {
 
       <div>
         <h2>Current Friends:</h2>
-        <FriendsList friends={users}></FriendsList>
+        {/* <FriendsList friends={users} variant={"current"}></FriendsList> */}
+        <FriendsList friends={friends.friends} variant={"current"}></FriendsList>
       </div>
 
       <div>
         <h2>Add Friends:</h2>
-        <FriendsList notFriends={users}></FriendsList>
+        <FriendsList friends={users}  variant={"send"}></FriendsList>
       </div>
 
       <div>
         <h2>Friend Requests:</h2>
-        <FriendsList notFriends={users}></FriendsList>
+        {/* <FriendsList friends={users} variant={"receive"}></FriendsList> */}
+        <FriendsList friends={friends.incoming_requests} variant={"receive"}></FriendsList>
       </div>
     </div>
   );
--- frontend/src/friends/FriendsList.jsx
+++ frontend/src/friends/FriendsList.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import '../util.js';
+import { Button } from "@mui/material";
 
+function VariantButtons({variant}){
+  switch (variant) {
+  case "current":
+    return <Button>Remove</Button>
+  case "receive":
+    return  <><Button>Accept</Button><Button>Ignore</Button></>
+  case "send":
+    return  <Button>Send</Button>
+  }
+}
 
-export default function FriendsList({friends}) {
+export default function FriendsList({friends, variant}) {
   var listItems;
   if (friends){
       listItems = friends.map(friend =>
-      <li>
-        <img
+      <li key={friend.id}>
+        {/* <img
           src={"https://fastly.picsum.photos/id/547/200/300.jpg?hmac=O1sHSqamP2AYNG_ADzB7uKiGjh_fmg-Xq4v2KEapg_k"}
           alt={friend.name}
-        />
+        /> */}
+        {friend.username}
+        <VariantButtons variant={variant}/>
       </li>
       )
   }
