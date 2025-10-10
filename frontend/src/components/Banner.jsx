32f7dfb794a098a7377eb276a46f61a6b876b889
David <dmetraux@isti.com>
2025-10-10 11:47:11 -0400

    have media query change logo and title size

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,6 +1,6 @@
 /*
 
-AI-generated: 10%: Used to get logout button
+AI-generated: 10%: Used to get logout button, used to conditionally render component sizes
 From Documentation: 30% (from MUI documentation: AppBar, Toolbar, Box usage patterns, basic layout examples)
 Human-written: 60% (logic: custom Banner component, Typography text, Logo placement, flex styling, color choices)
 
@@ -17,9 +17,11 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Logo from './Logo';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 import GroupIcon from '@mui/icons-material/Group';
 import HomeIcon from '@mui/icons-material/Home';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import {IconButton} from '@mui/material';
 
 import { useAuth } from '../auth/AuthContext';
@@ -27,8 +29,10 @@ import {Button} from '@mui/material';
 import { Link } from 'react-router-dom';
 import {Tooltip } from '@mui/material';
 
+
 function Banner(){
     const { auth } = useAuth();
+    const isMobile = useMediaQuery('(max-width:600px)');
 
     return (<Box 
           sx={{
@@ -40,10 +44,10 @@ function Banner(){
       <AppBar position="static" >
         <Toolbar>
             <div  style={{display: "flex", flexDirection: "row", justifyContent: "space-around", alignItems: "center", margin: "auto"}}>
-                <Typography align="center" variant="h4" component="div" color="rgb(66 33 111)">
+                <Typography align="center" style={isMobile ? {fontSize : "4vw"}: {}}  variant="h4"  component="div" color="rgb(66 33 111)">
                     PlanningJam
                 </Typography>
-                <Logo/>
+                <Logo style={isMobile ? {width : "30px"}: {}} />
             </div>
             {auth.accessToken && 
               <div>
@@ -55,6 +59,11 @@ function Banner(){
                   <Tooltip title="Friends">
                     <IconButton size="large"  color="inherit" component={Link} to="/friends" >
                       <GroupIcon />
+                    </IconButton>
+                  </Tooltip>
+                  <Tooltip title="User Profile">
+                    <IconButton size="large"  color="inherit" component={Link} to="/profile" >
+                      <AccountBoxIcon />
                     </IconButton>
                   </Tooltip>
                   <Tooltip title="Logout">
